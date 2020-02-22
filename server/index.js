cb76bc21f2815012acc11e2f173bf2d1eba8695c
Austin Abbott <magicaustin51547@gmail.com>
2020-02-22 16:17:00 -0700

    Render content to the page (localhost)

--- server/index.js
+++ server/index.js
@@ -1,3 +1,14 @@
 // create a server and route to handle the client get request by sending back
 // data on the requested animal in db.js
 const db = require('./db/db');
+
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => res.send('Hello World!'))
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
+
+ let mom = undefined;
